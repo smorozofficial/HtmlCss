2865499a10a049248d2f7f243538cd13c5700d7c
Moroz Sergey <Zwerew2@mail.ru>
2016-03-08 14:57:37 +0300

    script opacity

---
+++ src/HtmlCss/wwwroot/js/script1.js
@@ -0,0 +1,16 @@
+﻿var D = document.getElementById("centerLayer");
+var i = 0.0;
+var b = true;
+
+setInterval(function () {
+    if (D.style.opacity > 1.0)
+        b = false;
+    if (D.style.opacity < 0.1)
+        b = true;
+    if (b == true) {
+        i += 0.002;
+    } else {
+        i -= 0.002;
+    }
+    D.style.opacity = i.toString();
+}, 1);
